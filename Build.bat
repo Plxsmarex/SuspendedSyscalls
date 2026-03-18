5160a82be7cbd25863a84ce61dfa84e7ee363304
Plxsmarex <201103443+Plxsmarex@users.noreply.github.com>
2026-03-18 20:53:35 +0000

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,4 +1,3 @@
-@color 2
 @gcc -o SuspendedSyscalls.exe Main.c -T Linker.ld -e EntryPoint -Os -s -ffunction-sections -fno-asynchronous-unwind-tables -fno-ident -ffreestanding -nostartfiles -nostdlib -mno-stack-arg-probe
 @objcopy -j .text -O binary SuspendedSyscalls.exe SuspendedSyscalls.bin
 @pause
