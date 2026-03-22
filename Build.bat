5c360a8d0924bd2a22c6848d339d85780924ba9a
Plxsmarex <201103443+Plxsmarex@users.noreply.github.com>
2026-03-22 20:06:55 +0000

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,3 +1,3 @@
-@gcc -o SuspendedSyscalls.exe Main.c -T Linker.ld -e EntryPoint -Os -s -ffunction-sections -fno-asynchronous-unwind-tables -fno-ident -ffreestanding -nostartfiles -nostdlib -mno-stack-arg-probe
+@gcc -o SuspendedSyscalls.exe IndirectSyscaller.S Main.c -T Linker.ld -e EntryPoint -Os -s -ffunction-sections -fno-asynchronous-unwind-tables -fno-ident -ffreestanding -nostartfiles -nostdlib -mno-stack-arg-probe
 @objcopy -j .text -O binary SuspendedSyscalls.exe SuspendedSyscalls.bin
 @pause
