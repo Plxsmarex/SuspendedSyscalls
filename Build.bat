0c7991c941d57a39047a22e28785c329f7a7b56d
Plxsmarex <201103443+Plxsmarex@users.noreply.github.com>
2026-04-08 20:20:30 +0100

    Link to libc for embedded functions and the stack prober

--- Build.bat
+++ Build.bat
@@ -1,3 +1,3 @@
-@gcc -o SuspendedSyscalls.exe IndirectSyscaller.S Main.c -e EntryPoint -T Linker.ld -Oz -s -ffunction-sections -Wl,--gc-sections -fno-asynchronous-unwind-tables -nostartfiles -nostdlib -mno-stack-arg-probe
+@gcc -o SuspendedSyscalls.exe IndirectSyscaller.S Main.c -e EntryPoint -T Linker.ld -Oz -s -ffunction-sections -Wl,--gc-sections -fno-asynchronous-unwind-tables -nostartfiles
 @objcopy -j .text -O binary SuspendedSyscalls.exe SuspendedSyscalls.bin
 @pause
